cabcedbf34c9c751012dde85b4010054f1f71853
lolrepeatlol <alexei@asolonari.com>
2023-06-02 04:25:33 -0400

    Initial Commit

---
+++ Aspartame/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Aspartame
+//
+//  Created by Alexei Solonari on 6/2/23.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
